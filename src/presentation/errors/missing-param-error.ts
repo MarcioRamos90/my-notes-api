d67f0adbeb33039cb24cb147355d0df17cedcc59
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-01-25 22:49:29 -0300

    refactor: update MissingParamError message

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
   constructor (param: string) {
-    super(`No ${param} was provided`)
+    super(`Missing param: ${param}`)
     this.name = 'MissingParamError'
   }
 }
