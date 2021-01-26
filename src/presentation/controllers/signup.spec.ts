ca5df69815a8360cee13974480e74e65b5d5f08f
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-01-25 22:34:28 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,43 +1,51 @@
+import { HttpRequest } from '../protocols/http'
 import { SignUpController } from './signup'
+
+const fakeRequest = (body): HttpRequest => {
+  return { body }
+}
+interface SutTypes {
+  sut: SignUpController
+}
+const makeSut = (): SutTypes => {
+  const sut = new SignUpController()
+  return {
+    sut
+  }
+}
 
 describe('SignUpController', () => {
   it('Should SignUpController returns statusCode 400 if no email is provided', async () => {
-    const sut = new SignUpController()
-    const promise = await sut.handle({
-      body: {
-        password: 'any_password',
-        name: 'any_name'
-      }
-    })
-    expect(promise).toEqual({
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeRequest({
+      password: 'any_password',
+      name: 'any_name'
+    }))
+    expect(response).toEqual({
       statusCode: 400,
       body: new Error('No email was provided')
     })
   })
 
   it('Should SignUpController returns statusCode 400 if no name is provided', async () => {
-    const sut = new SignUpController()
-    const promise = await sut.handle({
-      body: {
-        password: 'any_password',
-        email: 'any_email'
-      }
-    })
-    expect(promise).toEqual({
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeRequest({
+      password: 'any_password',
+      email: 'any_email'
+    }))
+    expect(response).toEqual({
       statusCode: 400,
       body: new Error('No name was provided')
     })
   })
 
   it('Should SignUpController returns statusCode 400 if no password is provided', async () => {
-    const sut = new SignUpController()
-    const promise = await sut.handle({
-      body: {
-        name: 'any_name',
-        email: 'any_email'
-      }
-    })
-    expect(promise).toEqual({
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeRequest({
+      name: 'any_name',
+      email: 'any_email'
+    }))
+    expect(response).toEqual({
       statusCode: 400,
       body: new Error('No password was provided')
     })
