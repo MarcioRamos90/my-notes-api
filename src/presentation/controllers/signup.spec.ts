d582db9d35d966879568d834bd1bfabe109df308
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-01-25 22:26:36 -0300

    test: ensure SignUpController returns status 400 if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('SignUpController', () => {
       body: new Error('No email was provided')
     })
   })
+
+  it('Should SignUpController returns statusCode 400 if no name is provided', async () => {
+    const sut = new SignUpController()
+    const promise = await sut.handle({
+      body: {
+        password: 'any_password',
+        email: 'any_email'
+      }
+    })
+    expect(promise).toEqual({
+      statusCode: 400,
+      body: new Error('No name was provided')
+    })
+  })
 })
